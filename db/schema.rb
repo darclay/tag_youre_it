864798743ef011fcc3a1d2d0a32301b19819c593
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2022-05-04 17:47:57 -0500

    Creates, migrates UserTag join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_04_223821) do
+ActiveRecord::Schema.define(version: 2022_05_04_224513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(version: 2022_05_04_223821) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tag_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
