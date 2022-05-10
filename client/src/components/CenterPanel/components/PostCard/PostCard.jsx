faa1435a47c76d7e36375f59c45fa27263e0ce6a
David Gurule <davidgurulejr@gmail.com>
2022-05-10 14:36:24 -0600

    adds styling to the remaining postCard component; still need to figure out how to conditionally overlap members following icons

--- client/src/components/CenterPanel/components/PostCard/PostCard.jsx
+++ client/src/components/CenterPanel/components/PostCard/PostCard.jsx
@@ -14,16 +14,18 @@ export default function PostCard() {
 
 {/* Post Content */}
         <div className="postContent">
-          <h3 className="postTitle">Springfield Charity</h3>
-          <p className="postLocation">Springfield, NY</p>
-          <p className="postTime">1:30pm</p>
+          <div className="topBanner">
+            <h4 className="postTitle">Springfield Charity</h4>
+            <p className="postLocation">Springfield, NY</p>
+            <p className="postTime">1:30pm</p>
+          </div>
           <p className="postDate">04/24/2022</p>
           <p className="postDetails">Feugiat orci, magna sed vestibulum euismod velit ac dolor. Senectus nunc, eget morbi justo mi nisi congue. At vestibulum. Feugiat orci, magna sed vestibulum.</p>
         </div>
 
 {/* Top Right Tags */}
         <div className="tagRight">
-          <ul>
+          <ul className="tagsList">
             <li><img className="tagIcons" src="https://i.imgur.com/dBFN6Yd.png" alt="tag"/></li>
             <li><img className="tagIcons" src="https://i.imgur.com/RGgCa85.png" alt="tag"/></li>
             <li><img className="tagIcons" src="https://i.imgur.com/6gxMnrL.png" alt="tag"/></li>
@@ -34,13 +36,13 @@ export default function PostCard() {
 
  {/* Bottom joined banner */}
         <div className="joinContainer">
-          <button>Join Now</button>
+          <button className="joinButton">Join Now</button>
           <div className="thoseJoined">
             <p>3 People joined</p>
-            <ul>
-              <li><img src="#" alt="joined member" />pic</li>
-              <li><img src="#" alt="joined member" />pic</li>
-              <li><img src="#" alt="joined member" />pic</li>
+            <ul className="membersList">
+              <li><img className="memberIcons first" src="https://i.imgur.com/7hH5dsK.png" alt="joined member" /></li>
+              <li><img className="memberIcons second"  src="https://i.imgur.com/CfuyAtJ.png" alt="joined member" /></li>
+              <li><img className="memberIcons third"  src="https://i.imgur.com/vnZUCFM.png" alt="joined member" /></li>
             </ul>
           </div>
         </div>
