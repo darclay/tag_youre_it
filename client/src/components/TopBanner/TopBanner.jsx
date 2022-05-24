cfaf2e7fa35a2bf5a5319188b968bd3e2dcaa9c0
David Gurule <davidgurulejr@gmail.com>
2022-05-24 10:21:36 -0600

    adds styling to component and special styling to search bar placeholder.

--- client/src/components/TopBanner/TopBanner.jsx
+++ client/src/components/TopBanner/TopBanner.jsx
@@ -5,15 +5,15 @@ import settingsIcon from '../../assets/icons/settingsIconColored.png'
 export default function TopBanner() {
   return (
     <div className="topBannerDiv">
-      <div className="searchBar">
-        <input type="text" placeholder="Search Tags"/>
+      <div className="searchBarDiv">
+        <input className="searchBar" type="text" placeholder="Search Tags"/>
       </div>
       <div className="topBannerIconsContainer">
         <ul className="topBannerIcons">
           <li>
-            <img src={messageIcon} alt="message icon" />
+            <img className="topBannerIcon" src={messageIcon} alt="message icon" />
           </li>
-          <li><img src={settingsIcon} alt="settings icon" /></li>
+          <li><img className="topBannerIcon" src={settingsIcon} alt="settings icon" /></li>
         </ul>
       </div>
     </div>
